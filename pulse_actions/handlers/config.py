11de656ddde948939b5319e9533103d9f8ef39bb
Alice Scarpa <alicescarpa@gmail.com>
2015-06-25 15:39:09 -0300

    Basic automatic backfilling functionality.

--- pulse_actions/handlers/config.py
+++ pulse_actions/handlers/config.py
@@ -1,9 +1,15 @@
 import pulse_actions.handlers.treeherder_buildbot as treeherder_buildbot
+import pulse_actions.handlers.backfilling as backfilling
 
 HANDLERS_BY_EXCHANGE = {
     "exchange/treeherder/v1/job-actions": {
         "topic": {
             "buildbot": treeherder_buildbot.on_buildbot_event
+        }
+    },
+    "exchange/build/normalized": {
+        "topic": {
+            "unittest": backfilling.on_event
         }
     }
 }
